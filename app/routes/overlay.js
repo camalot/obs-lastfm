89d6d28f2d618d1d68b79e2452abed810f74febc
Ryan Conrad <camalot@gmail.com>
2018-11-25 11:37:51 -0600

    add support for imgur background video

--- app/routes/overlay.js
+++ app/routes/overlay.js
@@ -3,12 +3,17 @@ const express = require('express');
 const router = express.Router();
 const lastfm = require('../lib/lastfm');
 
-router.get('/:user', (req, res, next) => {
+router.get('/:user/:video?', (req, res, next) => {
 	let user = req.params.user;
+	let pvideo = req.params.video;
+	let video = {
+		url: pvideo ? `https://i.imgur.com/${pvideo}.mp4` : null,
+		enabled: pvideo ? true : false
+	}
 	lastfm.getTracks(req.params.user)
 		.then((track) => {
 			console.log(track);
-			res.render("overlay", { user: user, track: track });
+			res.render("overlay", { user: user, track: track, video: video });
 		}).catch((err) => {
 			console.error(err);
 			return next(err);
